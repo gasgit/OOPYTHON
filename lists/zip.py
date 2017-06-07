4cdd516d89fd53bd4cd8e5ccc4be013b7b9a7a7a
gasgit <glengardiner@gmail.com>
2017-06-08 00:15:40 +0100

    zip

---
+++ lists/zip.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+
+'''
+    zip function returns list of tuples from elements in n position from lists
+'''
+index = [1, 2, 3]
+fname = ['jack', 'humpty', 'ozzy']
+lname = ['white', 'dumpty', 'osbourne']
+
+ziped_names = zip(index, fname, lname)
+
+print ziped_names
