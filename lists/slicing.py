d9b431a8905f1d1131971b5d3c9dab9587b02fe0
gasgit <glengardiner@gmail.com>
2017-06-28 23:20:57 +0100

    more lists

---
+++ lists/slicing.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+
+'''
+    create myList
+'''
+
+myList = ['T', 'e', 's', 't', 'L', 'i', 's', 't']
+
+print myList[:], 'print entire list'
+
+print myList[0:4], 'slice between index 0 - 4'
+
+print myList[1:3], 'remove first and print next 2'
+
+print myList[:4], 'print all before index 4'
+
+print myList[4:], '    print all from index 4'
+
+print myList[-2:], 'print last 2'
+
+print myList[:-2], 'print before last 2'
---
+++ lists/strides.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+
+
+'''
+    create list using range
+'''
+
+myList = list(range(50))
+
+
+print myList, 'entire list'
+
+print myList[:20:4], 'stride of 4 from 0 to 20'
+
+print myList[::5], 'stride for complete list in 5'
